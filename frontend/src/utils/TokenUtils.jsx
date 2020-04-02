c6a3a15b5c8c04971863795284506ee907dc204d
Viktor Naumik <viktorna27@gmail.com>
2020-04-03 00:45:32 +0300

    Teams begin

--- frontend/src/utils/TokenUtils.jsx
+++
@@ -1,9 +0,0 @@
-import {ACCESS_TOKEN} from './Constants.jsx';
-
-export const setToken = (token) => {
-  window.localStorage.setItem(ACCESS_TOKEN, token);
-};
-
-export const getToken = () => {
-  return window.localStorage.getItem(ACCESS_TOKEN);
-};
